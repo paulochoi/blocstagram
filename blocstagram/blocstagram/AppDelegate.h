e43f5692be1ca6c73f946ab68fff657134809bdb
Paulo Choi <makwanza@gmail.com>
2015-06-29 09:52:23 -0400

    First Commit

---
+++ blocstagram/blocstagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  blocstagram
+//
+//  Created by Paulo Choi on 6/29/15.
+//  Copyright (c) 2015 Paulo Choi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ blocstagram/blocstagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  blocstagram
+//
+//  Created by Paulo Choi on 6/29/15.
+//  Copyright (c) 2015 Paulo Choi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
