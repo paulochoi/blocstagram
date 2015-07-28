5642c7dd714d1d835e9eabd4f7b2a5784f5c1eef
Paulo Choi <makwanza@gmail.com>
2015-07-27 22:55:46 -0400

    Complete full screen pics

---
+++ blocstagram/blocstagram/MediaFullScreenViewController.h
@@ -0,0 +1,23 @@
+//
+//  MediaFullScreenViewController.h
+//  blocstagram
+//
+//  Created by Paulo Choi on 7/27/15.
+//  Copyright (c) 2015 Paulo Choi. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+@class Media;
+
+@interface MediaFullScreenViewController : UIViewController
+
+@property (nonatomic,strong) UIScrollView *scrollView;
+@property (nonatomic,strong) UIImageView *imageView;
+
+- (instancetype) initWithMedia:(Media *)media;
+
+- (void) centerScrollView;
+
+@end
--- blocstagram/blocstagram/MediaTableViewCell.m
+++ blocstagram/blocstagram/MediaTableViewCell.m
@@ -12,7 +12,7 @@
 #import "User.h"
 
 
-@interface MediaTableViewCell()
+@interface MediaTableViewCell() <UIGestureRecognizerDelegate>
 
 @property (nonatomic,strong) UIImageView *mediaImageView;
 @property (nonatomic,strong) UILabel *usernameAndCaptionLabel;
@@ -20,6 +20,10 @@
 @property (nonatomic,strong) NSLayoutConstraint *imageHeightConstraint;
 @property (nonatomic,strong) NSLayoutConstraint *usernameAndCaptionLabelHeightConstraint;
 @property (nonatomic,strong) NSLayoutConstraint *commentLabelHeightConstraint;
+
+@property (nonatomic,strong) UITapGestureRecognizer *tapGestureRecognizer;
+@property (nonatomic,strong) UILongPressGestureRecognizer *longPressGestureRecognizer;
+
 
 @end
 
@@ -138,6 +142,16 @@ static NSParagraphStyle *paragraphStyle;
     
     if (self){
         self.mediaImageView = [UIImageView new];
+        self.mediaImageView.userInteractionEnabled = YES;
+        
+        self.tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapFired:)];
+        self.tapGestureRecognizer.delegate = self;
+        [self.mediaImageView addGestureRecognizer:self.tapGestureRecognizer];
+        
+        self.longPressGestureRecognizer = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(longPressFired:)];
+        self.longPressGestureRecognizer.delegate = self;
+        [self.mediaImageView addGestureRecognizer:self.longPressGestureRecognizer];
+        
         self.usernameAndCaptionLabel = [UILabel new];
         self.usernameAndCaptionLabel.numberOfLines = 0;
         self.usernameAndCaptionLabel.backgroundColor = usernameLabelGray;
@@ -172,6 +186,20 @@ static NSParagraphStyle *paragraphStyle;
         
     }
     return self;
+}
+
+- (void) tapFired:(UITapGestureRecognizer *) sender{
+    [self.delegate cell:self didTapImageView:self.mediaImageView];
+}
+
+- (void) longPressFired: (UILongPressGestureRecognizer *)sender {
+    if (sender.state == UIGestureRecognizerStateBegan) {
+        [self.delegate cell:self didLongPressImageView:self.mediaImageView];
+    }
+}
+
+- (BOOL) gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldReceiveTouch:(UITouch *)touch{
+    return self.isEditing == NO;
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
