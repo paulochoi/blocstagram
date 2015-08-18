18635cbc62f5afbd4039ca954d716f27a2d261cf
Paulo Choi <makwanza@gmail.com>
2015-08-18 17:28:53 -0400

    Complete filter assignment

---
+++ blocstagram/blocstagram/FilterCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  FilterCollectionViewCell.h
+//  blocstagram
+//
+//  Created by Paulo Choi on 8/18/15.
+//  Copyright (c) 2015 Paulo Choi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FilterCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ blocstagram/blocstagram/FilterCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  FilterCollectionViewCell.m
+//  blocstagram
+//
+//  Created by Paulo Choi on 8/18/15.
+//  Copyright (c) 2015 Paulo Choi. All rights reserved.
+//
+
+#import "FilterCollectionViewCell.h"
+
+@implementation FilterCollectionViewCell
+
+@end
